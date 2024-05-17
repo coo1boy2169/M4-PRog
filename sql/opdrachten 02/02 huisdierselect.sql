5a7798f62a860eaad06b2c10aba3148fb5161dca
Coo1boy2169 <>
2024-05-17 10:02:49 +0200

    02 slecect

---
+++ sql/opdrachten 02/02 huisdierselect.sql
@@ -0,0 +1,3 @@
+SELECT naam FROM `huisdieren` WHERE 1
+SELECT * FROM `huisdieren` WHERE 1
+SELECT naam,eigenaar FROM `huisdieren` WHERE 1
---
+++ sql/opstarten 1/huisdierendata.sql
@@ -0,0 +1,8 @@
+INSERT INTO `huisdieren` ( `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`, `naam`) 
+VALUES 
+('Coolboy', 'Yorkie', 3, '2021-05-17', 'Beni'),
+( 'playboy', 'houndu', 69, '1999-04-21', 'pikachu'),
+( 'feni', 'dido', 6, '1954-04-21', 'k8'),
+( 'keni', 'goldretriver', 65, '2001-04-21', 'k9'),
+( 'bensel', 'bronzretriver', 9, '1899-04-21', 'goldhond'),
+( 'playgirl', 'silerretriver', 61, '1998-04-21', 'cahrmentder');
