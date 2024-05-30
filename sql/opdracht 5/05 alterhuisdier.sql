a9dbeea64a55e3cded6afc9e645875cf75e440d2
Coo1boy2169 <>
2024-05-30 12:54:58 +0200

    opdracht 4 05

--- sql/opdracht 5/05 alterhuisdier.sql
+++ sql/opdracht 5/05 alterhuisdier.sql
@@ -1,2 +1,4 @@
 ALTER TABLE huisdieren DROP COLUMN eigenaar;
 ALTER TABLE huisdieren ADD eigenaar INT NOT NULL;
+
+
---
+++ sql/opdracht 5/05 slect.sql
@@ -0,0 +1,5 @@
+SELECT  FROM `huisdieren` WHERE naam = Beni ;
+SELECT  FROM `huisdiereigenaar` WHERE id = 1;
+
+SELECT  FROM `huisdieren` WHERE naam = goldhond ;
+SELECT  FROM `huisdiereigenaar` WHERE id = 4 ;
