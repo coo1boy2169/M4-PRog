9a33fdcdbc5c9ee3a80ab29a79ce868d47f9a1ec
Coo1boy2169 <>
2024-05-30 12:31:17 +0200

    opdracht 1 05

---
+++ sql/opdracht 5/05 eigenaar.sql
@@ -0,0 +1,8 @@
+INSERT INTO `huisdiereigenaar` (`id`, `voornaam`, `achternaam`, `leeftijd`)
+ VALUES 
+ (NULL, 'palyboy', 'bunny', '16'),
+ (NULL, 'palygirl', 'rabit', '15'),
+ (NULL, 'feni', 'beni', '69'),
+
+ 
+ 
